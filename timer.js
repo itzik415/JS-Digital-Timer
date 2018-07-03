949de4d447b3af8ddba7a44db98f22a94bbc8ad3
Itzik Shaoulian <itzikshaoulian@gmail.com>
2018-07-03 18:25:29 +0300

    fixing the timer

--- timer.js
+++ timer.js
@@ -1,8 +1,11 @@
 
-document.getElementById('timeChoseSeconds').value = 0;
-document.getElementById('timeChoseMinutes').value = 0;
-document.getElementById('timerSeconds').innerHTML = '00';
-document.getElementById('timerMinutes').innerHTML = '00';
+let startingPosition = () => {
+    document.getElementById('timeChoseSeconds').value = 0;
+    document.getElementById('timeChoseMinutes').value = 0;
+    document.getElementById('timerSeconds').innerHTML = '00';
+    document.getElementById('timerMinutes').innerHTML = '00';
+}
+startingPosition();
 
 var countInt;
 var pressed = 1;//we manipulate this variable to protect 
@@ -15,28 +18,32 @@ function start(pressed){
     submitButton.addEventListener("click", ()=>{
         var seconds = document.getElementById('timeChoseSeconds').value
         var minutes = document.getElementById('timeChoseMinutes').value
-        document.getElementById('timerSeconds').innerHTML = ("0" + seconds).slice(-2);
-        document.getElementById('timerMinutes').innerHTML = ("0" + minutes).slice(-2);
-        counter(parseInt(minutes), parseInt(seconds), pressed)
-        pressed += 1
-        console.log(pressed)
+        if (seconds > 59 || minutes > 59) {
+            alert('You entered wrong numbers. Please try again');
+            startingPosition();
+        }else {
+            document.getElementById('timerSeconds').innerHTML = ("0" + seconds).slice(-2);
+            document.getElementById('timerMinutes').innerHTML = ("0" + minutes).slice(-2);
+            counter(parseInt(minutes), parseInt(seconds), pressed)
+            pressed += 1
+        }
+        
     })
 }
         
 var counter = function(minutes, seconds, pressed){
-    console.log('received from addEventListener', pressed)
     if (pressed === 1 ){
-        console.log(minutes, seconds, pressed)
         countInt = setInterval(()=>{
-            if (seconds != 0){
+            if (seconds > 59 || minutes > 59) {
+                clearInterval(countInt)
+                pressed = 1;
+                startingPosition();
+                start(pressed=1)
+            }else if (seconds != 0){
                 seconds -= 1
             }else if(seconds == 0 && minutes != 0){
                 seconds = 59
                 minutes -= 1
-            }else{
-                console.log('we need to reset interval');
-                clearInterval(countInt);
-                start(pressed = 1);
             }
                 document.getElementById('timerSeconds').innerHTML = ("0" + seconds).slice(-2);
                 document.getElementById('timerMinutes').innerHTML = ("0" + minutes).slice(-2);   
@@ -49,9 +56,6 @@ restartBut.addEventListener("click", ()=>{
     console.log('restart button pressed')
     clearInterval(countInt)
     pressed = 1;
-    document.getElementById('timeChoseSeconds').value = 0;
-    document.getElementById('timeChoseMinutes').value = 0;
-    document.getElementById('timerSeconds').innerHTML = '00';
-    document.getElementById('timerMinutes').innerHTML = '00';
+    startingPosition();
     start(pressed=1)
 })
